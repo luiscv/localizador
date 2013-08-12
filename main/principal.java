7c9eb75c237098dc9e315027ced505ecb12fe8c7
luiscv <luiscv86@gmail.com>
2013-08-12 19:15:06 -0300

    Create principal.java

---
+++ main/principal.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package main;
+import controle.ctrl;
+/**
+ *
+ * @author luis
+ */
+public class principal {
+
+     public static void main(String[] args) {
+        
+          ctrl programa = new ctrl();
+          programa.iniciar();
+        
+    }
+    
+}
